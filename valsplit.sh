ea70b82f5ed42d29b147cd42d0f326a2fea2bfd0
AsadKhan163631 <34480579+AsadKhan163631@users.noreply.github.com>
2019-04-03 14:01:17 +0100

    Added assingments

--- valsplit.sh
+++ valsplit.sh
@@ -1,24 +1,30 @@
 # Take two input files, if not provided create missing input.txt, correct.txt, incorrect.txt
 # input.txt - mips instructions to validate
 # correct.txt - valid instructions
-# incorrect.txt - errounous instructions
+# incorrect.txt - erroneous instructions
 #
 #      ---- instruction specs (Things that need validation) ---
 # Must be one of add, sub, addi, lw, sw
 # add, sub, addi -> requires 3 args e.g add $s0 $s1 $s2
 # lw, sw -> requires 2 args e.g lw $t1 8( $t2 )
-# Register must start with $ and followed by sN ( 7 => N => 0) and tN ( 9 => N => 0)
-# Immediates (the second source operand of the addi instruction and the offset in the lw and sw instructions) have a range N where 0 => N => 65535
-# The second parameter for lw and sw instructions contains both an immediate and a register e.g8( $t2 )
+# Register must start with $ and followed by sN (7 => N => 0) and tN (9 => N => 0)
+# Immediate (the second source operand of the addi instruction and the offset in the lw and sw instructions) have a range N where -32768 => N => 32767
+# The second parameter for lw and sw instructions contains both an immediate and a register e.g 8( $t2 )
 #
 #      ---- implementation details ---
-# functions return nothing if the are successfull otherwise the return an error string
-# erronous instructions will be written to error file only in the validation loop same with vaild instructions
-# printing of error messages to stdout/terminal is done only in validatin loop
+# functions return nothing if the are successful otherwise the return an error string
+# returns are done through stdout e.g. echo or printf
+# erroneous instructions will be written to error file only in the validation loop same with vaild instructions
+# printing of error messages to stdout/terminal is done only in validation loop
 #
 #      ----- TODO -----
-# cleanup code
-# cleanup name space (use local where possible)
+# clean-up code
+# clean-up name space (use local where possible)
+#
+#      ----- Assingments ---
+# Jimmy - File validation
+# Rob - Validate Register and Immediate
+# Asad - Validate memory location, Validate instruction, main validataion loop and optimisations
 
 
 ADD="add"
@@ -33,56 +39,54 @@ error_file="$3" # third arg
 
 validate_instruction(){
   instruction=("$@") # array of instructions elements
-  ops=("${instruction[@]:1}") # oprand sublist
+  ops=("${instruction[@]:1}") # operand sub-list
 
   # checks if operation is add or sub
   if [ "${instruction[0]}" == "$ADD" ] || [ "${instruction[0]}" == "$SUB" ]; then
 
-    if [ "${#ops[@]}" == 3 ]; then # checks if it has 3 oprands
-      for i in "${ops[@]}" # loops through oprands
+    if [ "${#ops[@]}" == 3 ]; then # checks if it has 3 operands
+      for i in "${ops[@]}" # loops through operands
       do
 
         echo $(validate_register "$i") # check each register is valid
 
       done
-    else # oprands != 3
-      echo "Not enough oparands looking for 3" # print error message
+    else # operands != 3
+      echo "Not enough operands looking for 3" # print error message
       return # script breaking error if left
     fi
 
 
   elif [ "${instruction[0]}" == "$SW" ] || [ "${instruction[0]}" == "$LW" ]; then # checks if operation is sw or lw
       size="${#ops[@]}"
-      if [ "$size" == 4 ]; then   # checks if it has 4 oprands e.g [$s0, 4(, $t0, )]
+      if [ "$size" == 4 ]; then   # checks if it has 4 operands e.g. [$s0, 4(, $t0, )]
 
-        echo $(validate_register "${ops[0]}") # checks if first oprand is a valid register
-        echo $(validate_memlocation "${ops[@]:1}") # checks if second oprand is a valid memlocation
+        echo $(validate_register "${ops[0]}") # checks if first operand is a valid register
+        echo $(validate_memlocation "${ops[@]:1}") # checks if second operand is a valid memlocation
 
-      else # oprands != 2
-        echo "Not enough oparands looking for 2" # print error message
-        return # script breaking
+      else # operands != 2
+        echo "Not enough operands looking for 2" # print error message
+        return # script breaking error must return
       fi
 
   elif [ "${instruction[0]}" == "$ADDI" ]; then # checks if operation is addi
 
-      if [ "${#ops[@]}" == 3 ]; then # checks if it has 2 oprands
+      if [ "${#ops[@]}" == 3 ]; then # checks if it has 2 operands
 
-        echo $(validate_register "${ops[0]}") # checks if first oprand is a valid register
-        echo $(validate_register "${ops[1]}") # checks if second oprand is a valid register
-        echo $(validate_imediate "${ops[2]}") # checks if third oprand is a valid immediate
+        echo $(validate_register "${ops[0]}") # checks if first operand is a valid register
+        echo $(validate_register "${ops[1]}") # checks if second operand is a valid register
+        echo $(validate_imediate "${ops[2]}") # checks if third operand is a valid immediate
       fi
 
   else # operation not recognised
-
     echo "Invalid operation ${instruction[0]} in $line on line $line_count" # print error message
-    return
   fi
 }
 
 validate_register(){
   local reg="$1"
 
-  if [ "${#reg}" -lt 3 ]; then # checks if register is correct length > 3
+  if [ "${#reg}" -lt 3 ]; then # checks if string is correct length > 3 e.g $t0
     echo "Unexpected size expected (size > 3) got ${#reg} in $reg in $line on line $line_count"
     return # script breaking error must return
   fi
@@ -91,7 +95,7 @@ validate_register(){
     echo "Token not found \$ in $reg in $line on line $line_count"
   fi
 
-  local name="${reg:1:1}"
+  local name="${reg:1:1}" # register name
   if [ "$name" == t ]; then
 
     if [ "${reg:2}" -gt 9 ] || [ "${reg:2}" -lt 0 ]; then
@@ -109,7 +113,7 @@ validate_register(){
   fi
 }
 
-validate_imediate(){ # takes one value
+validate_imediate(){ # takes one value e.g -9 or 70000
   local value="$1"
   if [ "$value" -gt 32767 ] || [ "$value" -lt -32768 ]; then
     echo "Invalid immediate range, expected between -32768,32767 got $value in $line on line $line_count"
@@ -121,7 +125,7 @@ validate_memlocation(){ # takes array e.g [4(, $t0, )]
 
   if [ "${#parts[@]}" != 3 ] ; then
     echo "Invalid memory location format $parts in $line on line $line_count"
-    return # script breaking
+    return # script breaking must return
   fi
 
   local imediate_size="${#parts[0]}"
@@ -135,7 +139,7 @@ validate_memlocation(){ # takes array e.g [4(, $t0, )]
 if [ -z "${instruction_file}" ]; then # check instruction file
   echo "Input file not specified"
   echo "Usage: ./valsplit.sh input.txt correct.txt (optional) incorrect.txt (optional)"
-  exit -1
+  exit -1 # exit script
 fi
 
 if [[ -z "${output_file}" ]]; then # check output file
@@ -150,22 +154,22 @@ if [[ -z "${error_file}" ]]; then # check error file
   error_file="incorrect.txt"
 fi
 
-line_count=0
+line_count=0 # initial line
 
-while IFS= read line
+while IFS= read line # loop through lines
 do
 
   IFS=' ' read -r -a array <<< "$line" # split line by space
 
   ret=$(validate_instruction "${array[@]}") # validate each instruction
 
-  if [ ! -z "$ret" ]; then
-    echo "$ret" # print excpetion message to terminal
-    echo "$line" >> "${error_file}" # append erronous instruction to error file
+  if [ ! -z "$ret" ]; then # if there was an error message
+    echo "$ret" # print exception message to terminal
+    echo "$line" >> "${error_file}" # append erroneous instruction to error file
   else
-    echo "$line" >>  "${output_file}" # append valid instruction to output file
+    echo "$line" >> "${output_file}" # append valid instruction to output file
   fi
 
   line_count=$((line_count + 1)) # increment line count
 
-done <"$instruction_file"
+done < "$instruction_file"
